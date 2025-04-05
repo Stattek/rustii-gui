042e50220d4bfa7037dd2826472b2b251e615b6b
Stattek <stattek1@pm.me>
2025-04-05 04:12:30 -0500

    Remove some warnings.

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,10 @@ use iced::widget::{
 use iced::{Center, Element, Fill, Subscription, Theme};
 use iced::{Font, Task, keyboard};
 use rascii_art::RenderOptions;
-use rascii_art::charsets::{Charset, MINIMAL};
+use rascii_art::charsets::MINIMAL;
 use rustii::ascii_image_options::AsciiImageOptions;
 use rustii::convert_image_to_ascii_png;
-use std::io;
-use std::path::{Path, PathBuf};
-use std::sync::Arc;
+use std::path::PathBuf;
 
 pub fn main() -> iced::Result {
     iced::application("RustiiGUI", RustiiGui::update, RustiiGui::view)
@@ -42,8 +40,6 @@ struct RustiiGui {
 enum Message {
     ThemeChanged(Theme),
     InputChanged(String),
-    InputFile,
-    OutputFile,
     Convert,
     OpenInputFile,
     OpenOutputFile,
